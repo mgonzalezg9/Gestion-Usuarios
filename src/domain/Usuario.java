26ff3102d44f041e048213878c6a3264ae82d715
Manuel Gonzalez <mgonzalez4648@gmail.com>
2019-07-11 18:37:02 +0200

    Objeto Usuario creado

--- src/domain/Usuario.java
+++ src/domain/Usuario.java
@@ -1,5 +1,30 @@
 package domain;
 
 public class Usuario {
+	private int id_usuario;
+	private String usuario, password;
 
+	public Usuario(int id_usuario, String usuario, String password) {
+		this.id_usuario = id_usuario;
+		this.usuario = usuario;
+		this.password = password;
+	}
+
+	public int getId_usuario() {
+		return id_usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " {" + "id = " + id_usuario + ", usuario = " + usuario + ", password = "
+				+ password + "}";
+	}
 }
