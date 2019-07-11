5091a91b6c726bbcce4b88309d5fc8e3d16caaf0
Manuel Gonzalez <mgonzalez4648@gmail.com>
2019-07-11 19:43:19 +0200

    Metodos a falta de rellenar
    Falta por implementar la funcionalidad de conexion a la base de datos y
    el menu de gestion de usuarios.

--- src/datos/Conexion.java
+++ src/datos/Conexion.java
@@ -1,5 +1,32 @@
 package datos;
 
-public class Conexion {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class Conexion {
+	// TODO Atributos de clase
+	private final static String JDBC_STRING;
+	private final static String JDBC_URL;
+	private final static String JDBC_USER;
+	private final static String JDBC_PASS;
+
+	// Métodos de acceso a la base de datos
+	public static Connection getConnection() {
+		// TODO
+		
+		return null;
+	}
+
+	public static void close(ResultSet rs) {
+		// TODO
+	}
+
+	public static void close(PreparedStatement ps) {
+		// TODO
+	}
+	
+	public static void close(Connection c) {
+		// TODO
+	}
 }
--- src/datos/UsuariosJDBC.java
+++ src/datos/UsuariosJDBC.java
@@ -1,5 +1,53 @@
 package datos;
 
-public class UsuariosJDBC {
+import java.sql.Connection;
+import java.util.List;
 
+import domain.Usuario;
+
+public class UsuariosJDBC {
+	// TODO Atributos de clase
+	private static final String SQL_INSERT;
+	private static final String SQL_UPDATE;
+	private static final String SQL_DELETE;
+	private static final String SQL_SELECT;
+	
+	// Atributos
+	private Connection userConn;
+	
+	// Constructores
+	public UsuariosJDBC() {
+		userConn = Conexion.getConnection();
+	}
+	
+	public UsuariosJDBC(Connection c) {
+		userConn = c;
+	}
+	
+	public int insert(String usuario, String password) {
+		// TODO
+		
+		return 0;
+	}
+	
+	public int update(int id_usuario, String usuario, String password) {
+		// TODO
+		
+		return 0;
+	}
+	
+	public int delete(int id_usuario) {
+		// TODO
+		
+		return 0;
+	}
+	
+	public List<Usuario> select() {
+		// TODO
+		
+		return null;
+	}
+	
+	
+	
 }
